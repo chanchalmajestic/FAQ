807a3aff5b78c752f71cacdf364f9c43abc9df0f
chanchalmajestic <chanchal@majestictechnologies.net>
2023-10-19 12:24:40 +0530

    test

--- resources/views/admin/faqform.blade.php
+++ resources/views/admin/faqform.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel FAQ Form</title>
+    <title>Laravel   FAQ Form</title>
     <style>
          *{
             font-family:sans-serif;
